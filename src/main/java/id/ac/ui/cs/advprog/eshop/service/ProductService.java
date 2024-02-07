98a7e48179c5293781fce002cb62ddcc2d35f2c1
pershyfon <sabrina.aviana@ui.ac.id>
2024-02-07 12:02:39 +0700

    functional test create, edit, & delete product

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,4 +6,7 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
+    public Product findById(String id);
+    public Product editProduct(Product product);
+    public Product deleteProduct(Product product);
 }
